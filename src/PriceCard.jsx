4829a15867a4d3d17d1be0d8e6e9f94a050cab95
INFANTJOY <infantjoy123@gmail.com>
2024-02-22 10:11:01 +0530

    Initial

---
+++ src/PriceCard.jsx
@@ -0,0 +1,22 @@
+// src/PriceCard.js
+function PriceCard({ title, price, features }) {
+    return (
+      <div className="price-card">
+        <h2 style={{color: "gold"}}>{title}</h2>
+        <p className="price">{price}</p>
+        <hr />
+        <ul>
+          {features.map((feature, index) => (
+            <li className="details" key={index}>
+              {feature.available ? '✓' : '✗'} {feature.name}
+            </li>
+          ))}
+        </ul>
+        <button>Buy Now</button>
+      </div>
+    );
+  }
+  
+  
+  export default PriceCard;
+  
